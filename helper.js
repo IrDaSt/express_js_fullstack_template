adaa7598f6c69872c91bd9553e0eef4666681f36
Daniel W <danielwj00@gmail.com>
2021-07-10 03:30:00 +0800

    helper for date time format

--- helper.js
+++ helper.js
@@ -15,8 +15,25 @@ const encryptMD5 = (text) => {
   return crypto.createHash("md5").update(text).digest("hex");
 };
 
+const formatDate = (date) => {
+  var date = new Date(date);
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var dt = date.getDate();
+
+  if (dt < 10) {
+    dt = "0" + dt;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  return year + "-" + month + "-" + dt;
+};
+
 module.exports = {
   emptyOrRows,
   ecryptSHA256,
   encryptMD5,
+  formatDate,
 };
