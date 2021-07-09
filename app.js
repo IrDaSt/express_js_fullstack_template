edb3eda60f38062b0b2356e0a14bbaeef42d7546
Daniel W <danielwj00@gmail.com>
2021-07-10 02:22:23 +0800

    Notes for helmet

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
   })
 );
 app.use(upload.array());
-app.use(helmet());
+// app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/web/index.js
+++ routes/web/index.js
@@ -7,12 +7,13 @@ router.use("/users", usersRouter);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res
-    .set(
-      "Content-Security-Policy",
-      "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'; font-src *; img-src 'self' data:; frame-src 'self' http://* 'unsafe-inline';"
-    )
-    .render("pages/index", { title: "Express" });
+  // res
+  //   .set(
+  //     "Content-Security-Policy",
+  //     "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'; font-src *; img-src 'self' data:; frame-src 'self' http://* 'unsafe-inline';"
+  //   )
+  //   .render("pages/index", { title: "Express" });
+  res.render("pages/index", { title: "Express" });
 });
 
 module.exports = router;
