195bf7cd030d9bca0a9cb529d7da6e675514b530
Daniel W <danielwj00@gmail.com>
2021-08-06 22:54:30 +0800

    mongoose populate functions

--- models/Phones.js
+++ models/Phones.js
@@ -1,18 +1,34 @@
 const mongoose = require("mongoose");
 
-const PhonesSchema = new mongoose.Schema({
-  phone_number: {
-    type: String,
-    required: true,
+const PhonesSchema = new mongoose.Schema(
+  {
+    phone_number: {
+      type: String,
+      required: true,
+    },
+    phone_area: {
+      type: String,
+      required: true,
+    },
+    phone_owner: {
+      type: String,
+      required: true,
+    },
   },
-  phone_area: {
-    type: String,
-    required: true,
-  },
-  phone_owner: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+// POPULATE FUNCTIONS
+// MessagesSchema.set("toObject", { virtuals: true });
+// MessagesSchema.set("toJSON", { virtuals: true });
+
+// MessagesSchema.virtual("sender_data", {
+//   ref: "Users",
+//   localField: "sender_id",
+//   foreignField: "_id",
+//   justOne: true,
+// });
 
 module.exports = mongoose.model("Phones", PhonesSchema);
