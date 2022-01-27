e3419edf4fdc31f21f962b734f72196979baa292
Daniel W <danielwj00@gmail.com>
2022-01-27 11:16:40 +0800

    ensure

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,9 +1,11 @@
 const multer = require("multer");
 const path = require("path");
+const fse = require("fs-extra");
 
 // Handle file upload temporary location
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: async function (req, file, cb) {
+    await fse.ensureDir("./public/data/uploads/temp");
     cb(null, "./public/data/uploads/temp");
   },
   filename: function (req, file, cb) {
