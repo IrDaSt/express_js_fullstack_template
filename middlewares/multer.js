7816ceb306efc3578472ef194501d162f4f8b8f5
Daniel W <danielwj00@gmail.com>
2022-06-02 18:18:17 +0800

    fix multer

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,21 +1,23 @@
-const multer = require("multer");
-const path = require("path");
-const fse = require("fs-extra");
+const multer = require("multer")
+const path = require("path")
+const fse = require("fs-extra")
+const idGeneratorUtils = require("../utilities/id-generator.utils")
 
 // Handle file upload temporary location
 const storage = multer.diskStorage({
   destination: async function (req, file, cb) {
-    await fse.ensureDir("./public/data/uploads/temp");
-    cb(null, "./public/data/uploads/temp");
+    await fse.ensureDir("./public/data/uploads/temp")
+    cb(null, "./public/data/uploads/temp")
   },
   filename: function (req, file, cb) {
     cb(
       null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
+      `${file.fieldname}-${idGeneratorUtils.generateUUIDV4()}` +
+        `-${Date.now()}${path.extname(file.originalname)}`,
+    )
   },
-});
+})
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage })
 
-module.exports = upload;
+module.exports = upload
